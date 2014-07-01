b827d7bef90b1b01dc2d89a609c0d3229045814f
Jarno van Leeuwen <vanleeuwen.jarno@gmail.com>
2014-07-01 15:21:23 +0200

    Clean up

--- runall.sh
+++ runall.sh
@@ -3,53 +3,54 @@
 buildgram=true
 compile=true
 rungunit=true
-runjasmine=true
+runjasmin=true
 generatecode=true
 
 while getopts 'agcrj' flag; do
-  case "${flag}" in
-    a) buildgram=false ;;
-    c) compile=false ;;
-    r) rungunit=false ;;
-    j) runjasmine=false ;;
-    g) generatecode=false ;;
-    *) error "Unexpected option ${flag}" ;;
-  esac
+    case "${flag}" in
+        a) buildgram=false ;;
+        c) compile=false ;;
+        r) rungunit=false ;;
+        j) runjasmin=false ;;
+        g) generatecode=false ;;
+        *) error "Unexpected option ${flag}" ;;
+    esac
 done
 
 # cp tests/* bin/
 
 if [[ $buildgram = true ]]; then
-echo "(a) Building Antlr grammars"
-cd src/vb/stil/; java org.antlr.Tool *.g; cd ../../..;
+    echo "(a) Building ANTLR grammars"
+    cd src/vb/stil/; java org.antlr.Tool *.g; cd ../../..;
 fi
 # java org.antlr.Tool src/vb/stil/*.g
 
 
 if [[ $compile = true ]]; then
-echo "(c) Compiling..."
-# TODO: make this platform agnostic
-java -jar /Applications/eclipse/plugins/org.eclipse.jdt.core_*.jar -d bin/ -1.7 src/
+    echo "(c) Compiling..."
+    # TODO: make this platform agnostic
+    java -jar /Applications/eclipse/plugins/org.eclipse.jdt.core_*.jar -d bin/ -1.7 src/
 fi
 
 if [[ $rungunit = true ]]; then
-echo "(r) Running gunit tests..."
-echo "==== GUNIT ===="
-cd bin; java org.antlr.gunit.Interp ../gunit/Stil.gunit
-java org.antlr.gunit.Interp ../gunit/StilChecker.gunit
-cd ../
-echo "==== END ===="
+    echo "(r) Running GUnit tests..."
+    echo "==== GUNIT ===="
+    cd bin; java org.antlr.gunit.Interp ../gunit/Stil.gunit
+    java org.antlr.gunit.Interp ../gunit/StilChecker.gunit
+    cd ../
+    echo "==== END ===="
 fi
+
 if [[ $generatecode = true ]]; then
-echo "(g) Generating code"
-java -Dfile.encoding=UTF-8 -classpath bin vb.stil.Stil gunit/program.st
+    echo "(g) Generating code"
+    java -Dfile.encoding=UTF-8 -classpath bin vb.stil.Stil gunit/program.st
 fi
 
-if [[ $runjasmine = true ]]; then
-echo "(j) Compiling with jasmine"
-java -jar jasmin.jar -d gen/ gen/program.j 
+if [[ $runjasmin = true ]]; then
+    echo "(j) Compiling with Jasmin"
+    java -jar jasmin.jar -d gen/ gen/program.j 
 
-echo "==== RUNNING PROGRAM ===="
-cd gen; java Program
-echo "====      END        ===="
+    echo "==== RUNNING PROGRAM ===="
+    cd gen; java Program
+    echo "====      END        ===="
 fi
